017dc8900001741d3c8462c84c6a52a3f01b469b
swamprat <92648744+convict-real@users.noreply.github.com>
2023-06-12 16:05:01 -0500

    Made custom epsilon

--- main.c
+++ main.c
@@ -4,16 +4,17 @@
 #include <Windows.h>
 #include <string.h>
 
+#define EPSILON 1e-7f
+
 int main()
 {
     float number = 0.f;
+    int decimal_places = (int)log10(1 / EPSILON);
     
     while (1) {
-        while (number < 1.f) {           
-            printf("%f\n", number += FLT_EPSILON);
-        }
+        while (number < 1.f) printf("%.*f\n", decimal_places, number += EPSILON);
         
-        if (number == 1.f) {
+        if (number >= 1.f) {
             system("pause");
             return 0;
         }
